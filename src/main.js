9b5d070a0301a61a18b5b21eaaeecca0cc5bb7a7
BlancaCarrasco <blankcarr@gmail.com>
2019-01-31 18:01:52 -0300

    enlace con firebase

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-html
+import {checkAuthState, registerUser} from '../auth/auth.js';
+
+window.onload = () =>{
+  checkAuthState((user)=>{
+    if(user){
+      loginOrRegister.style.display = "none";
+      app.style.display = "block";
+
+    }else{
+      loginOrRegister.style.display = "block";
+      app.style.display = "none";
+
+    }
+  });
+};
+const registerWithEmailAnPasword =()=>{
+  const emailformUser = emailTextfiel.value;
+  const passwordformUser = passwordTextfiel.value;
+  registerUser(emailformUser,passwordformUser);
+};
+registerButton.addEventListener('click', registerWithEmailAnPasword);
+
+
+
