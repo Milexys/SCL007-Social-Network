ad81feb874aad0e0360efbd1ff9020ea1842e972
BlancaCarrasco <blankcarr@gmail.com>
2019-01-31 22:22:40 -0300

    agregando carpetas borradas

---
+++ src/auth/auth.js
@@ -0,0 +1,21 @@
+export const checkAuthState=(callback) => {
+  firebase.auth().onAuthStatechanged((user)=>{
+    if (user){
+    console.log("hay usuario>"+JSON.stringify(user));
+    callback(user)
+    }else{
+      console.log("no esta logueado")
+      callback(null)
+    }
+  })    
+};
+
+export const registerUser = (email, password) => {
+  firebase.auth().createUserWhithEmailAndPassword(email,password)
+  .then((user)=>{
+    console.log("ususrio registrado>" + JSON.stringify(user));
+    })
+    .cath((error)=>{
+      console.error("Error>"+error.message);
+  });
+}
