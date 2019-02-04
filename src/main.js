4c718426e91448e0892fc3ceff5274c78dd00c25
BlancaCarrasco <blankcarr@gmail.com>
2019-02-04 10:06:39 -0300

    registro de usuario

--- src/main.js
+++ src/main.js
@@ -1,30 +1,49 @@
-import {checkAuthState, registerUser} from './auth/auth.js';
+function registrar(){
+  // console.log ("hiciste click");
+  const email= document.getElementById("mailtext").value; 
+  const contraseña= document.getElementById("passwordTextfield").value; 
+  firebase.auth().createUserWithEmailAndPassword(email, contraseña).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    
+  });
+}
+function login(){
+  //console.log ("hiciste click");
+}
+function logout(){
+  //console.log ("hiciste click");
+}
+// import {checkAuthState}from './auth/auth.js';import {registerUser} from './auth/auth.js';
 
-window.onload = () => {
-  checkAuthState((user)=>{
-    if(user){
-      loginOrRegister.style.display = "none";
-      app.style.display = "block";
-    }else{
-      loginOrRegister.style.display = "block";
-      app.style.display = "none"; 
-    }})
-  checkAuthState((user)=>{
-    if(user){
-      logout.style.display = "none";
-      app.style.display = "block";
-    }else{
-      logout.style.display = "block";
-      app.style.display = "none";
-      }    
-  })
-};
+// window.onload = () => {
+//   checkAuthState((user)=>{
+//     if(user){
+//       loginOrRegister.style.display = "none";
+//       app.style.display = "block";
+//     }else{
+//       loginOrRegister.style.display = "block";
+//       app.style.display = "none"; 
+//     }})
+//   checkAuthState((user)=>{
+//     if(user){
+//       logout.style.display = "none";
+//       app.style.display = "block";
+//     }else{
+//       logout.style.display = "block";
+//       app.style.display = "none";
+//       }    
+//   })
 
 
-const registerWithEmailAndPassword = () => {
-  const emailFromUser = emailTextfield.value;
-  const passwordFromUser = passwordTextfield.value;
-  registerUser(emailFromUser, passwordFromUser);
-};
 
-registerButton.addEventListener('click', registerWithEmailAndPassword);
+// const registerWithEmailAndPassword = () => {
+//   const emailFromUser = emailTextfield.value;
+//   const passwordFromUser = passwordTextfield.value;
+//   registerUser(emailFromUser, passwordFromUser);
+// };
+
+// registerButton.addEventListener('click', registerWithEmailAndPassword);
+// };
