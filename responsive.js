07e279bf81222d8671eb202d2e638254079414d5
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-02-07 22:18:47 -0300

    separando archivos js

---
+++ responsive.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  let modal = document.getElementById("mimodal");
+  let flex = document.getElementById("flex");
+  let openModal = document.getElementById("register");
+  let closeModal = document.getElementById("close");
+   
+  openModal.addEventListener("click", () =>{
+  modal.style.display = "block";
+  });
+  closeModal.addEventListener("click", () =>{
+  modal.style.display = "none";
+  });
+  window.addEventListener("click", (e) =>{
+  if (e.target === flex){
+  modal.style.display = "none";
+  }
+  });
