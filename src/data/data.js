d8d148b161984f98d0944840b96567fc3409fea2
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-02-08 13:06:07 -0300

    post listo

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,25 @@
-export const savePet = (petOwner, petName, petType, petSex, petAge) => {
+/*export const savePet = (petOwner, petName, petType, petSex, petAge, petInformation) => {
   const newPetKey = firebase.database().ref('pets/').child('profile').push().key;
   firebase.database().ref(`pets/${newPetKey}`).set({
     Owner: petOwner,
     Name: petName,
     Type: petType,
     Sex: petSex,
-    Age: petAge
+    Age: petAge,
+    Information: petInformation
+  });
+}*/
+
+export const savePosting = (postText, userName) => {
+  const newPostKey = firebase.database().ref('post/').child('post').push().key;
+  firebase.database().ref(`post/${newPostKey}`).set({
+    user: userName,
+    posting: postText,
   });
 }
+export const readPost = (onPostChange) => {
+  var postRef = firebase.database().ref('post');
+  postRef.on('child_added', (post)=> {
+    onPostChange(post);
+  });
+};
